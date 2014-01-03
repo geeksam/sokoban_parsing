68372ac14a96810523e93bf5738cfaeef229b38d
Sam Livingston-Gray <geeksam@gmail.com>
2014-01-03 00:02:36 -0800

    Add some cell-locating methods to Level

--- lib/sokoban/level.rb
+++ lib/sokoban/level.rb
@@ -38,5 +38,29 @@ module Sokoban
     def [](x, y)
       rows[y][x]
     end
+
+    def neighbors_of(cell)
+      x, y = *coordinates_of(cell)
+
+      # FIXME: add boundary checking; technically, this wraps around.
+      # Fortunately, there's always a wall around the outside, so for
+      # purposes of just marking cells as reachable, it doesn't matter.
+      n = [ x + 0 , y + 1 ]
+      s = [ x + 0 , y - 1 ]
+      e = [ x + 1 , y + 0 ]
+      w = [ x - 1 , y + 0 ]
+
+      [n, e, s, w].compact.map {|x, y| self[x, y] }
+    end
+
+    def coordinates_of(cell)
+      y = rows.index {|row| row.include?(cell) }
+      x = rows[y].index(cell)
+      [x, y]
+    end
+
+    def starting_cell
+      rows.flatten.detect {|cell| cell.contents.kind_of?(Character) }
+    end
   end
 end
