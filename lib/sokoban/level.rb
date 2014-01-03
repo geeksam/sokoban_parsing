3fee18a9452a946b9ab67b2603d7b368b10c34c8
Sam Livingston-Gray <geeksam@gmail.com>
2014-01-02 23:21:02 -0800

    ...yeah, that turns out to be a giant pain in the butt.

--- lib/sokoban/level.rb
+++ lib/sokoban/level.rb
@@ -1,14 +1,33 @@
 module Sokoban
   class Level
     class Cell
-      def initialize(contents = nil)
+      def initialize(contents = nil, level = nil)
         @contents = contents
+        @level    = level
       end
 
-      attr_reader :contents
+      attr_reader :contents, :level
 
       def reachable?
-        contents.kind_of?(Character)
+        @reachable ||=
+          contents.kind_of?(Character) || neighbors.any?(&:reachable?) ||
+          :nope
+        return false if @reachable == :nope
+        @reachable
+      end
+
+      def in_level(level)
+        self.class.new(contents, level)
+      end
+
+      def neighbors
+        level.neighbors_of(self) #.tap {|x| require 'pp'; puts; pp x }
+      end
+
+      def inspect
+        s = self.class.to_s.split('::').last
+        s.upcase! if contents.kind_of?(Character)
+        s
       end
     end
 
@@ -30,13 +49,29 @@ module Sokoban
     class Crate; end
 
     def initialize(rows)
-      @rows = rows
+      @rows = rows.map {|row| row.map {|cell| cell.in_level(self) } }
     end
 
     attr_reader :rows
 
     def [](x, y)
       rows[y][x]
+    end
+
+    def neighbors_of(cell)
+      y = rows.index {|row| row.include?(cell) }
+      x = rows[y].index(cell)
+
+      cardinal_neighbors(x, y)
+    end
+
+    def cardinal_neighbors(x, y)
+      n = self[ x   , y-1 ]
+      e = self[ x+1 , y   ]
+      s = self[ x   , y+1 ]
+      w = self[ x-1 , y   ]
+
+      [n, e, s, w].compact
     end
   end
 end
--- spec/level_spec.rb
+++ spec/level_spec.rb
@@ -2,30 +2,45 @@ require_relative "../lib/sokoban/parser"
 
 describe Sokoban::Level do
   describe "reachability" do
-    let(:irregular_map) {
+    let(:bifurcated_map) {
       <<-END_MAP.gsub(/^\s*/, "")
-      ###
-      #@###
-      # $.#
-      #####
+      #########
+      #@ $.*# #
+      #########
       END_MAP
     }
-    let(:irregular_level) {
-      Sokoban::Parser.parse(irregular_map)
-    }
+    let(:bifurcated_level) { Sokoban::Parser.parse(bifurcated_map) }
 
     specify "a Wall is not reachable" do
-      expect(irregular_level[0, 0]).to_not be_reachable
+      expect(bifurcated_level[0, 0]).to_not be_reachable
     end
 
-    specify "a Void is not reachable" do
-      expect(irregular_level[3, 0]).to_not be_reachable
-    end
+    # specify "a Void is not reachable" do
+    #   expect(bifurcated_level[3, 0]).to_not be_reachable
+    # end
 
     specify "other Cells are reachable if they contain a Character" do
-      expect(irregular_level[1, 1]).to be_reachable
+      expect(bifurcated_level[1, 1]).to be_reachable
     end
 
-    specify "other Cells are reachable if any of their neighbors is reachable"
+    specify "other Cells are reachable if any of their neighbors is reachable" do
+require 'pp'; puts
+puts bifurcated_level.rows.map(&:inspect)
+puts bifurcated_level.rows.map {|row| row.map(&:reachable?).inspect }
+      expect(bifurcated_level[1, 1].contents).to be_an_instance_of(Sokoban::Level::Character)
+      expect(bifurcated_level[1, 1]).to be_reachable
+
+      expect(bifurcated_level[2, 1].contents).to be_nil
+      expect(bifurcated_level[2, 1]).to be_reachable
+
+      expect(bifurcated_level[3, 1].contents).to be_an_instance_of(Sokoban::Level::Crate)
+      expect(bifurcated_level[3, 1]).to be_reachable
+
+      expect(bifurcated_level[4, 1].contents).to be_nil
+      expect(bifurcated_level[4, 1]).to be_reachable
+
+      expect(bifurcated_level[6, 1].contents).to be_nil
+      expect(bifurcated_level[6, 1]).to_not be_reachable
+    end
   end
 end
